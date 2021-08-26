dc73d08b89a34b52d66b7c2abc1cfe037d40c371
Robin Bette <robinbette@hotmail.com>
2021-08-26 11:37:43 +0200

    Create category filter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { EMPTY } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { ProductService } from './product.service';
 
@@ -12,12 +12,23 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
+  selectedCategoryId = 1;
 
   products$ = this.productService.productsWithCategory$.pipe(
     catchError((err) => {
       this.errorMessage = err;
       return EMPTY;
     })
+  );
+
+  productSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+    map((products) =>
+      products.filter((product) =>
+        this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true
+      )
+    )
   );
 
   constructor(private productService: ProductService) {}
