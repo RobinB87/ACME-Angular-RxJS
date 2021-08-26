6b00a9bb4ee58bdeb963128f8ae893d0e29182db
Robin Bette <robinbette@hotmail.com>
2021-08-26 11:29:26 +0200

    Use combine latest

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, throwError } from 'rxjs';
+import { combineLatest, Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
-import { Supplier } from '../suppliers/supplier';
 import { SupplierService } from '../suppliers/supplier.service';
+import { ProductCategoryService } from './../product-categories/product-category.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,26 +16,45 @@ export class ProductService {
   private suppliersUrl = this.supplierService.suppliersUrl;
 
   products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    // To map over an array:
     // First map the emitted array
     // Then map each element in the array
     // Finally transform each array element
-    map((products) =>
+    // map((products) =>
+    //   products.map(
+    //     (product) =>
+    //       ({
+    //         ...product,
+    //         price: product.price * 1.5,
+    //         searchKey: [product.productName],
+    //       } as Product)
+    //   )
+    // ),
+    tap((data) => console.log('Products: ', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
+
+  productsWithCategory$ = combineLatest([
+    this.products$,
+    this.productCategoryService.productCategories$,
+  ]).pipe(
+    map(([products, categories]) =>
       products.map(
         (product) =>
           ({
             ...product,
             price: product.price * 1.5,
+            category: categories.find((c) => product.categoryId === c.id).name,
             searchKey: [product.productName],
           } as Product)
       )
-    ),
-    tap((data) => console.log('Products: ', JSON.stringify(data))),
-    catchError(this.handleError)
+    )
   );
 
   constructor(
     private http: HttpClient,
-    private supplierService: SupplierService
+    private supplierService: SupplierService,
+    private productCategoryService: ProductCategoryService
   ) {}
 
   getProducts(): Observable<Product[]> {
